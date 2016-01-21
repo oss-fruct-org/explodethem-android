4f1b22468d3c8d364b720d7ad42fe2466edec590
Kirill Ivashov <ivashov@cs.petrsu.ru>
2016-01-21 16:26:33 +0300

    Add AdwowFlavor stub for full version

---
+++ ExplodeThem/src/full/java/org/fruct/oss/explodethem/AdwowFlavor.java
@@ -0,0 +1,26 @@
+package org.fruct.oss.explodethem;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+
+public class AdwowFlavor {
+	public static void initializeAdwow(Application app) {
+	}
+
+	public static void setupFragment(Activity activity) {
+	}
+
+	public static void startAdwow(Activity activity) {
+	}
+
+	public static void stopAdwow(Activity activity) {
+	}
+
+	public static void saveMoment(final Context context, String name) {
+	}
+
+	public static void saveMoment(final Context context, String name, int number) {
+	}
+
+}
